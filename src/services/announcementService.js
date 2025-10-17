263184fb5f04838f2e315e241909f5eac4c73040
unknown <briviodarren@gmail.com>
2025-10-17 20:41:57 +0700

    feat: update API URL to live Back4App backend

--- src/services/announcementService.js
+++ src/services/announcementService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/announcements/';
+const API_URL = 'https://sshsbackend-wscb0c1s.b4a.run/api/users/';
 
 const getToken = () => {
   const user = JSON.parse(localStorage.getItem('user'));
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/users/';
+const API_URL = 'https://sshsbackend-wscb0c1s.b4a.run/api/users/';
 
 // Login user
 const login = async (userData) => {
